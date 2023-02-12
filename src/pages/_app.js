fcaef91581e386ebfb6f9279aea5143a7ba2053a
Jnanashish <jhandique1999@gmail.com>
2023-02-12 11:27:09 +0530

    css file import issue solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
+import "../styles/globals.css";
+
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
-
-import "../styles/globals.css";
 
 import { Provider } from "react-redux";
 import { store } from "../Redux/store";
