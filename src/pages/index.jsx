7f8ca667ce47e4b4c2a65dd165d2ab2e8bf7675c
Jnanashish Handique <jnanashishhandique@Jnanshish-Macbook-Pro.local>
2025-01-25 21:42:01 +0530

    import issue fixed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Router from "next/router";
-import Meta from "../core/SEO/Meta.jsx/index.js";
+import Meta from "../core/SEO/Meta.jsx";
 
 const Home = () => {
     // for now all routes are redirected to job listing page
